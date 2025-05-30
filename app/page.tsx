a64ae6f4c38d21c7701195410709df319ef8d4c5
PureloveS2 <loverl@tutamail.com>
2025-05-30 01:28:50 -0300

    First commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,79 @@
 import Image from "next/image";
+import Link from "next/link";
+import { FaGithub, FaTelegram } from "react-icons/fa";
+import { SiRoblox } from "react-icons/si";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main className="flex justify-center items-center h-screen">
+      <div className="bg-zinc-950 w-[95%] max-w-[500px] flex flex-col items-center p-4 rounded-2xl gap-4">
+        
+      <div className="flex w-full items-center gap-2">
+          
+          <hr className="text-zinc-500 w-full"/>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <p>Sobre</p>
+          
+          <hr className="text-zinc-500 w-full"/>
+          
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+
+        <figure className="text-center flex flex-col items-center gap-4">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src={"/profile-picture/lain.jpg"}
+            width={720}
+            height={720}
+            alt="Profile picture"
+
+            className="rounded-full w-[40%] border-2 border-white"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          
+          <figcaption className="w-[95%]">
+            <p className="font-bold text-xl mb-1 text-purple-500">Purelove</p>
+            <p>Desenvolvedor full-stack, apaixonado em criar código e atualmente aprendendo a fazer jogos no Roblox</p>
+          </figcaption>
+        </figure>
+        
+        <div className="flex w-full items-center gap-2">
+          
+          <hr className="text-zinc-500 w-full"/>
+
+          <p>Sociais</p>
+          
+          <hr className="text-zinc-500 w-full"/>
+          
+        </div>
+
+        <ul className="w-full flex flex-col gap-3">
+          <li>
+            <Link href={"https://t.me/PureloveS2"} target="_blank" className="flex items-center bg-blue-500 hover:bg-blue-400 active:bg-blue-300 rounded-full p-2 transition">
+              <FaTelegram className="absolute w-8 h-8" />
+              <p className="text-center w-full text-xl font-bold">Telegram</p>
+            </Link>
+          </li>
+
+          <li>
+            <Link href={"https://www.roblox.com/pt/users/7671700222/profile"} target="_blank" className="flex items-center bg-blue-700 hover:bg-blue-600 active:bg-blue-500 rounded-full p-2 transition">
+              <SiRoblox className="absolute w-8 h-8" />
+              <p className="text-center w-full text-xl font-bold">Roblox</p>
+            </Link>
+          </li>
+
+          <li>
+            <Link href={"https://github.com/PureloveS2"} target="_blank" className="flex items-center bg-gray-900 hover:bg-gray-800 active:bg-gray-700 rounded-full p-2 transition">
+              <FaGithub className="absolute w-8 h-8" />
+              <p className="text-center w-full text-xl font-bold">Github</p>
+            </Link>
+          </li>
+        </ul>
+
+        <hr className="text-zinc-500 w-full"/>
+        
+        <div>
+          <small className="text-zinc-300">© 2025 • Purelove :)</small>
+        </div>
+
+      </div>
+    </main>
   );
 }
